a9fadc1f51bf1bf7c8f449f31d06eecf3c1dd76d
Jenny <jenny369314@gmail.com>
2024-03-17 22:29:14 +0800

    SectionList練習

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StyleSheet, StatusBar, SafeAreaView } from 'react-native';
 import Header from "./src/components/Header";
 import AlbumList from "./src/components/AlbumList";
-import albumData from "./src/json/albums.json";
+//import albumData from "./src/json/albums.json";
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
-      <Header title={albumData.albumTitle} />
-      <AlbumList list={albumData.albumList} />
+      <Header />
+      <AlbumList />
     </SafeAreaView>
   );
 }
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,14 +1,16 @@
 //多張專輯卡的統整
-import { FlatList } from 'react-native';
+import { SectionList } from 'react-native';
 import AlbumDetail from "./AlbumDetail";
+import sections from "../json/album_section.json"
 
-const AlbumList = ({ list }) => { // list是從App.js傳過來json陣列
+const AlbumList = () => {
   const renderItem = ({ item }) => <AlbumDetail album={item} />; // 將資料再往下傳
   return (
-    <FlatList 
-        data={list} // json裡的陣列資料
+    <SectionList 
+        sections={sections} // json裡的陣列資料
+        // title 傳給 renderSectionHeade，data 陣列內的資料逐筆變成 item 傳給 renderItem
         renderItem={renderItem} //將 item 資料透過 album 屬性傳給 AlbumDetail.js
-        keyExtractor={item => item.title} //幫每個 item 資料設定 key
+        keyExtractor={item => item.title} //幫每個 item 資料設定 key，因為只有一個列表所以不用 index 參數...?
       />
   );
 }
