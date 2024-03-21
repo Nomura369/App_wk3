69d96c15863255ff852ca85df35e4e99dee69937
Jenny <jenny369314@gmail.com>
2024-03-21 22:49:32 +0800

    stack導覽練習-detail

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,20 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, Linking } from 'react-native';
+import { Button, Card, Text, PricingCard, Tile } from "react-native-elements";
 
 const DetailScreen = ({ route }) => {
+  const { title, artist, price, url, image, description } = route.params;
+  const pressed = () => Linking.openURL(url);
   return (
-    <View style={styles.container}>
-        <Text>{route.params.title}</Text>
-    </View>
+    <ScrollView>
+      <Tile
+        imageSrc={{ uri: image }}
+        featured
+        title={title.toUpperCase()}
+        caption={artist.toUpperCase()}
+      />
+      <PricingCard
+        color="#E53711"
+        title="Discount Now!"
+        price={`Price: $${price}`}
+        info={["1 User", "Basic Support", "All Core Features"]}
+        button={{ title: "BUY NOW"}}
+        onButtonPress={pressed}
+      />
+      <Card>
+        <Text>Artist: {artist}</Text>
+        <Text style={{ marginBottom: 10 }}>Title: {title}</Text>
+        <Text style={{ fontWeight: "bold" }}>Description</Text>
+        <Text style={{ marginBottom: 10 }}>{description}</Text>
+        <Button
+          raised
+          icon={{ name: "add-shopping-cart" }}
+          backgroundColor="#E53711"
+          title="ADD TO CART"
+          onPress={pressed}
+        />
+      </Card>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default DetailScreen;
